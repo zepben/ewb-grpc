027ec854a33507c198480dd974beb89d58eb9cb4
Kurt Greaves <kurt.greaves@zepben.com>
2020-09-29 14:17:05 +1000

    Add setup.py for pip editable install compatibility

---
+++ python/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+test_deps = ["pytest", "pytest-asyncio"]
+setup(
+    name="zepben.protobuf",
+    description="Evolve Python SDK",
+    version="0.3.0b2",
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src"),
+    setup_requires=[
+        "grpcio-tools",
+        "pep517"
+    ],
+    install_requires=[
+        "protobuf",
+        "grpcio",
+        "grpcio-tools",
+    ],
+    extras_require={
+        "test": test_deps,
+    }
+)
