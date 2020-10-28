c9c6a812fecfa69300626e80f4cc85b7c9208e48
Kurt Greaves <kurt.greaves@zepben.com>
2020-10-28 16:04:52 +1100

    Deploy python wheel to pypi
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_namespace_packages
 
-test_deps = ["pytest", "pytest-asyncio"]
 setup(
     name="zepben.protobuf",
-    description="Evolve Python SDK",
+    description="Evolve Python Protobuf and gRPC definitions",
     version="0.8.0b5",
+    url="https://github.com/zepben/evolve-sdk-python",
+    author="Kurt Greaves",
+    author_email="kurt.greaves@zepben.com",
+    license="Mozilla Public License v2.0",
+    classifiers=[
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent"
+    ],
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
+    python_requires='>=3.7',
     setup_requires=[
         "grpcio-tools",
         "pep517"
@@ -16,7 +26,4 @@ setup(
         "grpcio",
         "grpcio-tools",
     ],
-    extras_require={
-        "test": test_deps,
-    }
 )
