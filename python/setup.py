beda20e9335325e3a0756131f8ac8f8c1fbd82dc
CI <ci@zepben.com>
2021-09-22 14:03:31 +1000

    Update versions for release [skip ci]

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="zepben.protobuf",
     description="Evolve Python Protobuf and gRPC definitions",
-    version="0.16.0b3",
+    version="0.16.0",
     url="https://github.com/zepben/evolve-sdk-python",
     author="Kurt Greaves",
     author_email="kurt.greaves@zepben.com",
