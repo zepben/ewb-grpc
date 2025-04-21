b5ab2475bfa08b34061d0fc261d752b99b9c9956
Kurt Greaves <kurt.greaves@zepben.com>
2025-04-21 22:15:49 +1000

    [DEV-2895] Update protobuf and gRPC
    * Remove CSharp builds
    
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- python/setup.py
+++ python/setup.py
@@ -1,28 +1,6 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name="zepben.protobuf",
-    description="Evolve Python Protobuf and gRPC definitions",
-    version="0.35.0b2",
-    url="https://github.com/zepben/evolve-sdk-python",
-    author="Kurt Greaves",
-    author_email="kurt.greaves@zepben.com",
-    license="Mozilla Public License v2.0",
-    classifiers=[
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Operating System :: OS Independent"
-    ],
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
-    python_requires='>=3.9',
-    setup_requires=[
-        "grpcio-tools==1.59.3",
-        "pep517"
-    ],
-    install_requires=[
-        "protobuf==4.24.2",
-        "grpcio==1.59.3",
-    ],
 )
